d367e5259f9785f176d61ca989f72df44bd78bc6
Jon Turney <jon.turney@dronecode.org.uk>
2018-01-02 20:20:32 +0000

    Install more prerequisites for framework tests on Cygwin

--- ci/appveyor-install.bat
+++ ci/appveyor-install.bat
@@ -8,13 +8,29 @@ if not exist %CACHE% mkdir %CACHE%
 
 echo Updating Cygwin and installing ninja and test prerequisites
 %CYGWIN_ROOT%\%SETUP% -qnNdO -R "%CYGWIN_ROOT%" -s "%CYGWIN_MIRROR%" -l "%CACHE%" -g -P ^
+flex,^
+gcc-fortran,^
 gcc-objc++,^
 gcc-objc,^
 gobject-introspection,^
+libQt5Core-devel,^
+libQt5Gui-devel,^
+libSDL2-devel,^
 libboost-devel,^
 libglib2.0-devel,^
 libgtk3-devel,^
+itstool,^
+libllvm-devel,^
+libllvm-devel-static,^
+libncurses-devel,^
+libopenmpi-devel,^
+libprotobuf-devel,^
+libwmf-devel,^
+nasm,^
 ninja,^
+python3-cython,^
+python3-devel,^
+python3-gi,^
 python3-pip,^
 vala,^
 zlib-devel
