6286db0c1ff034c03fa2c365b6cbe6a5551696ce
Jon Turney <jon.turney@dronecode.org.uk>
2018-01-05 22:08:40 +0000

    CI: Try a Docker container
    Run meson CI in a Windows Docker container with Cygwin and test
    prerequisites pre-installed, rather than installing those prerequisites into
    the Appveyor VM each time.

--- ci/appveyor_cygwin.bat
+++ ci/appveyor_cygwin.bat
@@ -1,24 +1,13 @@
-if _%arch%_ == _x64_ set CYGWIN_ROOT=C:\cygwin64
-if _%arch%_ == _x86_ set CYGWIN_ROOT=C:\cygwin
-set PATH=%CYGWIN_ROOT%\bin;%SYSTEMROOT%\system32
-
 goto %1
 
 :install
-if _%arch%_ == _x64_ set SETUP=setup-x86_64.exe
-if _%arch%_ == _x86_ set SETUP=setup-x86.exe
-
-set PKGCACHE=C:\cache
-set CYGWIN_MIRROR="http://cygwin.mirror.constant.com"
-
-%CYGWIN_ROOT%\%SETUP% -qnNdO -R "%CYGWIN_ROOT%" -s "%CYGWIN_MIRROR%" -l "%PKGCACHE%" -g -P "ninja,gcc-objc,gcc-objc++,libglib2.0-devel,zlib-devel,python3-pip"
-
+docker pull --disable-content-trust jturney/mesonci-cygwin
 goto :eof
 
 :build_script
-ninja --version
+docker run jturney/mesonci-cygwin 'ninja --version'
 goto :eof
 
 :test_script
-env -- python3 run_tests.py --backend=%backend%"
+docker run -v %APPVEYOR_BUILD_FOLDER%:%APPVEYOR_BUILD_FOLDER% -w %APPVEYOR_BUILD_FOLDER% jturney/mesonci-cygwin 'python3 run_tests.py --backend=%backend%'
 goto :eof
