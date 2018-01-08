ce322ff10bb7f20cfb71a76878b8369307addf58
Jon Turney <jon.turney@dronecode.org.uk>
2018-01-08 22:22:00 +0000

    Don't use a bind mount to container, just copy files in
    (Bind mounts are slow on Windwos, as they use SMB?)
    XXX: need to use a bind mount to get results out???

--- ci/appveyor_cygwin.bat
+++ ci/appveyor_cygwin.bat
@@ -2,12 +2,14 @@ goto %1
 
 :install
 docker pull --disable-content-trust jturney/mesonci-cygwin
+echo FROM jturney/mesonci-cygwin >Dockerfile
+echo ADD %APPVEYOR_BUILD_FOLDER% %APPVEYOR_BUILD_FOLDER% >>Dockerfile
+docker built -t withgit .
 goto :eof
 
 :build_script
-docker run jturney/mesonci-cygwin 'ninja --version'
 goto :eof
 
 :test_script
-docker run -v %APPVEYOR_BUILD_FOLDER%:%APPVEYOR_BUILD_FOLDER% -w %APPVEYOR_BUILD_FOLDER% jturney/mesonci-cygwin 'python3 run_tests.py --backend=%backend%'
+docker run -w %APPVEYOR_BUILD_FOLDER% withgit 'python3 run_tests.py --backend=%backend%'
 goto :eof
