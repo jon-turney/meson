4e5083e3119654aea8db7a49a5d64d3d3e9e97a0
Jon Turney <jon.turney@dronecode.org.uk>
2018-11-28 00:09:31 +0000

    Use defaultlib to link with libcmt and oldnames
    XXX: this will need to take note of mt/debug options???
    XXX: not sure why we need this

--- mesonbuild/compilers/linker.py
+++ mesonbuild/compilers/linker.py
@@ -84,7 +84,13 @@ class VisualStudioLinker(Linker):
             return ['link']
 
     def get_linker_always_args(self):
-        return ['/nologo']
+        always = ['/nologo']
+
+        # clang doesn't emit directives that clang-cl does ???
+        if self.compiler.id == 'clang':
+            always += ['/defaultlib:libcmt', '/defaultlib:oldnames']
+
+        return always
 
     def get_linker_output_args(self, outputname):
         return ['/OUT:' + outputname]
