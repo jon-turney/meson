9e685ee8ceee0f8bf7772aeee73dc8f5d62d8819
Jon Turney <jon.turney@dronecode.org.uk>
2020-02-12 19:13:43 +0000

    Check before compiler detection if 'c' language is present when adding 'vala'
    For the sake of a consistent error message (irrespective of if 'valac' is
    present or not), check if the 'c' language is present if we are adding
    'vala' before (rather than after) we do compiler detection.

--- mesonbuild/interpreter.py
+++ mesonbuild/interpreter.py
@@ -3037,6 +3037,12 @@ external dependencies (including libraries) must go to "dependencies".''')
         return success
 
     def add_languages_for(self, args, required, for_machine: MachineChoice):
+        langs = set(self.coredata.compilers[for_machine].keys())
+        langs.update(args)
+        if 'vala' in langs:
+            if 'c' not in langs:
+                raise InterpreterException('Compiling Vala requires C. Add C to your project languages and rerun Meson.')
+
         success = True
         for lang in sorted(args, key=compilers.sort_clink):
             lang = lang.lower()
@@ -3070,11 +3076,6 @@ external dependencies (including libraries) must go to "dependencies".''')
                 logger_fun(comp.get_display_language(), 'linker for the', machine_name, 'machine:',
                            mlog.bold(' '.join(comp.linker.get_exelist())), comp.linker.id, comp.linker.version)
             self.build.ensure_static_linker(comp)
-
-        langs = self.coredata.compilers[for_machine].keys()
-        if 'vala' in langs:
-            if 'c' not in langs:
-                raise InterpreterException('Compiling Vala requires C. Add C to your project languages and rerun Meson.')
 
         return success
 
